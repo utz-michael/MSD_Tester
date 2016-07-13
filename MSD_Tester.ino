78790ce204b1b080d93e701b2a0bf468f2c7db5a
Michael Utz <utzm@kabelbw.de>
2016-07-13 22:35:25 +0200

    Version 1

---
+++ MSD_Tester.ino
@@ -0,0 +1,53 @@
+
+#include <Wire.h>  
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+ 
+int analogPin = 0;
+const int ledPin =  13;      // the number of the LED pin
+int ledState = LOW;             // ledState used to set the LED
+unsigned long previousmicros = 0;        // will store last time LED was updated
+long interval = 1;           // interval at which to blink (milliseconds)
+long RPM = 1000; 
+long RPM_old = 0;
+void setup() {
+  lcd.begin(16,2);
+  pinMode(ledPin, OUTPUT);
+ lcd.setCursor(0,0);  
+ lcd.print("MSD Tester");
+ lcd.setCursor(0,1);
+ lcd.print("           U/Min");
+}
+
+void loop() {
+  RPM = analogRead(analogPin)*10;
+  interval =1000000.0/(RPM * 0.2);
+
+  if ( RPM >= RPM_old + 21 || RPM <= RPM_old - 21 ){
+  lcd.setCursor(0,1);
+  lcd.print("           U/Min");
+  if (0 <= RPM && RPM <= 9){lcd.setCursor(8,1);}
+  if (10 <= RPM && RPM <= 99){lcd.setCursor(7,1);}
+  if (100 <= RPM && RPM <= 999){lcd.setCursor(6,1);}
+  if (1000 <= RPM && RPM <= 9999){lcd.setCursor(5,1);}
+  if (10000 <= RPM && RPM <= 99999){lcd.setCursor(4,1);}
+  lcd.print(RPM);
+  RPM_old = RPM;
+  }
+  unsigned long currentmicros = micros();
+
+  if (currentmicros - previousmicros >= interval && ledState == HIGH) {
+  
+    previousmicros = currentmicros;
+      ledState = LOW;
+    digitalWrite(ledPin, ledState);
+  }
+  if (currentmicros - previousmicros >= 2 * interval && ledState == LOW) {
+  
+    previousmicros = currentmicros;
+      ledState = HIGH;
+    digitalWrite(ledPin, ledState);
+  }
+
+  
+}
