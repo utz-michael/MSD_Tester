3234cc394269e2d26be288bf7a3d0edec2978bbf
Michael Utz <utzm@kabelbw.de>
2016-07-14 13:26:29 +0200

    tone anstelle timer

--- MSD_Tester.ino
+++ MSD_Tester.ino
@@ -2,60 +2,47 @@
 #include <Wire.h>  
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
- 
+ //LiquidCrystal_I2C lcd(0x27,16,2);
 int analogPin = 0;
 const int ledPin =  13;      // the number of the LED pin
-int ledState = LOW;             // ledState used to set the LED
-unsigned long previousmicros = 0;        // will store last time LED was updated
-long interval = 1;           // interval at which to blink (milliseconds)
-long RPM = 1000; 
-long RPM_old = 0;
-int analog = 0;
-int analog_old = 0;
+
+
+int RPM = 67; 
+int RPM_old = 0;
+int RPM_LCD = 1005;
+
 void setup() {
+ // lcd.init(); 
+  lcd.backlight();
   lcd.begin(16,2);
   pinMode(ledPin, OUTPUT);
  lcd.setCursor(0,0);  
  lcd.print("MSD Tester");
  lcd.setCursor(0,1);
  lcd.print("           U/Min");
+  Serial.begin(9600);
 }
 
 void loop() {
-  //RPM = analogRead(analogPin)*10;
-  analog = constrain (analogRead(analogPin),1,1000);
-  if ( analog >= analog_old + 10 || analog <= analog_old - 10 ){
-    RPM = analog * 10;
-  interval =1000000.0/(RPM * 0.2);
-  analog_old = analog;
-  }
-  
-
-  if ( RPM >= RPM_old + 99 || RPM <= RPM_old - 99 ){
+ RPM = map (analogRead(analogPin),0,1023,31,667);
+ 
+Serial.print(RPM*15); 
+Serial.print(" U/Min  ");
+Serial.print(RPM); 
+Serial.println(" Hz");
+  if ( RPM >= RPM_old + 1 || RPM <= RPM_old - 1 ){
   lcd.setCursor(0,1);
   lcd.print("           U/Min");
-  if (0 <= RPM && RPM <= 9){lcd.setCursor(8,1);}
-  if (10 <= RPM && RPM <= 99){lcd.setCursor(7,1);}
-  if (100 <= RPM && RPM <= 999){lcd.setCursor(6,1);}
-  if (1000 <= RPM && RPM <= 9999){lcd.setCursor(5,1);}
-  if (10000 <= RPM && RPM <= 99999){lcd.setCursor(4,1);}
-  lcd.print(RPM);
+  RPM_LCD = RPM * 15;
+  if (0 <= RPM_LCD && RPM_LCD <= 9){lcd.setCursor(8,1);}
+  if (10 <= RPM_LCD && RPM_LCD <= 99){lcd.setCursor(7,1);}
+  if (100 <= RPM_LCD && RPM_LCD <= 999){lcd.setCursor(6,1);}
+  if (1000 <= RPM_LCD && RPM_LCD <= 9999){lcd.setCursor(5,1);}
+  if (10000 <= RPM_LCD && RPM_LCD <= 99999){lcd.setCursor(4,1);}
+  lcd.print(RPM_LCD);
   RPM_old = RPM;
   }
-  unsigned long currentmicros = micros();
-
-  if (currentmicros - previousmicros >= interval && ledState == HIGH) {
-  
-    previousmicros = currentmicros;
-      ledState = LOW;
-    digitalWrite(ledPin, ledState);
-  }
-  if (currentmicros - previousmicros >= 2 * interval && ledState == LOW) {
-  
-    previousmicros = currentmicros;
-      ledState = HIGH;
-    digitalWrite(ledPin, ledState);
-  }
+  tone(ledPin,RPM);
 
   
 }
